e7a72e0ca3db5c321964d395140314d69fd666ee
Ulises <ulisesapaza.280@gmail.com>
2025-05-02 11:50:04 -0300

    chore: add example ch04

---
+++ ch04/examples/IfDemo.java
@@ -0,0 +1,11 @@
+public class IfDemo {
+    public static void main(String[] args) {
+        int j = 0;
+
+        if (j < 0) {
+            System.out.println("j is less than 0! Resetting");
+        } else {
+            System.out.println("j is greater than 0. Continuing.");
+        }
+    }
+}
